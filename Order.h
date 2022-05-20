b7ee2d685ceec6e7f7b5aff92f236dd4faa15f73
IT21294952 <99532670+IT21294952@users.noreply.github.com>
2022-05-20 16:46:06 +0000

    Comments given

--- Order.h
+++ Order.h
@@ -6,16 +6,16 @@ private:l
   double Order_Amount;
   char Order_Date[8];
   char Customer_ID[5];
-  Customer *Cust;
+  Customer *Cust; //the Customer ID assigned to each order placed by a particular customer
 
 public:
-  Order();
-  Order(char OID[], double Oamt, char ODate[], Customer *Cust);
-  void setOrderID(char[]);
-  char getOrderID();
-  void setOrder_Amount(double Oamt);
-  double getOrder_Amount();
-  void setODate(char[]);
-  char getODate();
-  ~Order();
+  Order(); //default constructor
+  Order(char OID[], double Oamt, char ODate[], Customer *Cust); //overloaded constructor
+  void setOrderID(char[]); //setter for Order ID
+  char getOrderID(); //getter for Order ID
+  void setOrder_Amount(double Oamt); //setter for Order Amount
+  double getOrder_Amount(); //getter for Order Amount
+  void setODate(char[]); //setter for Order Date
+  char getODate(); //getter for Order Date
+  ~Order(); //Destructor
 };
